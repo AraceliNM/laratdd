1772d1742ccda2d63b4fc5cfafcdb3bfe67165f2
araceli <araceli292.nm@gmail.com>
2023-01-09 19:36:29 +0000

    Fin Paso 31

--- resources/lang/en/users.php
+++ resources/lang/en/users.php
@@ -5,4 +5,16 @@ return [
         'admin' => 'Administrator',
         'user' => 'User',
     ],
+    'filters' => [
+        'roles' => [
+            'all' => 'Rol',
+            'admins' => 'Administradores',
+            'users' => 'Usuarios'
+        ],
+        'states' => [
+            'all' => 'Todos',
+            'active' => 'Solo activos',
+            'inactive' => 'Solo inactivos',
+        ]
+    ]
 ];
